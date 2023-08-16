e3178450c584a738d76e333e6512a1b65f070998
Dawyid22 <119014306+Dawyid22@users.noreply.github.com>
2023-08-16 16:15:04 -0300

    ""

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -4,28 +4,28 @@ const { conn, Country } = require('./src/db.js');
 const PORT = 3001;
 
 conn.sync({ force: false }).then(() => {
-server.listen(PORT, async () => { 
+  server.listen(PORT, async () => {
 
-  const DB = await Country.findAll()
-  if(!DB.length){
-    const { data } = await axios('http://localhost:5000/countries')
-    const SearchDB = data.map(country => {
-      const lenArray = country.languages ? Object.values(country.languages) : []
-      const lenguas = lenArray.join(", ")
-      return {
-        id: country.cca3,
-        name: country.name.common,
-        flag: country.flags.png,
-        continent: country.region,
-        capital: country.capital ? country.capital[0] : "The country has no capital",
-        population: country.population,
-        maps: country.maps ? country.maps.googleMaps : "No maps",
-        lenguajes: lenguas
-      }
-    })
+    const DB = await Country.findAll()
+    if (!DB.length) {
+      const { data } = await axios('http://localhost:5000/countries')
+      const SearchDB = data.map(country => {
+        const lenArray = country.languages ? Object.values(country.languages) : []
+        const lenguas = lenArray.join(", ")
+        return {
+          id: country.cca3,
+          name: country.name.common,
+          flag: country.flags.png,
+          continent: country.region,
+          capital: country.capital ? country.capital[0] : "The country has no capital",
+          population: country.population,
+          maps: country.maps ? country.maps.googleMaps : "No maps",
+          lenguajes: lenguas
+        }
+      })
 
-   await Country.bulkCreate(SearchDB)    
-  }
-  console.log(`Server listening on port ${PORT}`);
-})
+      await Country.bulkCreate(SearchDB)
+    }
+    console.log(`Server listening on port ${PORT}`);
+  })
 }).catch(error => console.error(error))
--- cr-pi-countries-main/server/src/routes/routeCountries.js
+++ cr-pi-countries-main/server/src/routes/routeCountries.js
@@ -1,13 +1,12 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const routerCountries = Router();
 const getAllCountries = require('../controllers/getAllCountries')
 const getCoutriesById = require('../controllers/getCountryById')
 const getCountriesByName = require('../controllers/getCountryByName')
 
-routerCountries.get('/', getAllCountries)
-
-routerCountries.get('/name', getCountriesByName)
-
-routerCountries.get('/:idPais', getCoutriesById)
+routerCountries
+    .get('/', getAllCountries)
+    .get('/name', getCountriesByName)
+    .get('/:idPais', getCoutriesById)
 
 module.exports = routerCountries
