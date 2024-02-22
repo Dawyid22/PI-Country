1372299f7789e42bd8e894f9709cd031ca0dd5cd
Dawyid22 <dawyid43@gmail.com>
2024-02-21 22:00:01 -0300

    acomodando la pagina

--- cr-pi-countries-main/client/src/App.jsx
+++ cr-pi-countries-main/client/src/App.jsx
@@ -1,25 +1,18 @@
 import './App.css'
 import { Route, Routes} from 'react-router-dom'
-import Landing from './components/Landing/Landing'
 import Form from './components/Form/Form'
 import Detail from './components/Detail/Detail'
 import NavBar from './components/NavBar/NavBar'
 import Cards from './components/Cards/Cards'
 import Activities from './components/Activities/Activities'
-import { useLocation } from 'react-router-dom'
+
 
 function App() {
- const location = useLocation()
   return (
       <div className='app_main'>
-        {location.pathname !== '/' ? <NavBar/> : null}
-        <div>
-          
-        </div>
-        
+         <NavBar/> 
         <Routes>
-          <Route path='/' element={<Landing/>}/>
-          <Route path='/home' element={<Cards/>}/>
+          <Route path='/' element={<Cards/>}/>
           <Route path='/form' element={<Form/>}/>
           <Route path='/detail/:id' element={<Detail/>}/>
           <Route path='/activities' element={<Activities/>}/>
--- cr-pi-countries-main/client/src/components/NavBar/NavBar.jsx
+++ cr-pi-countries-main/client/src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div className={style.containerNavBar}>
       <div className={style.containerLanding}>
-      <NavLink to={'/home'} className={style.buttonLanding}><img src='https://www.gifcen.com/wp-content/uploads/2021/04/earth-day-gif-11.gif' alt='Landing'
+      <NavLink to={'/'} className={style.buttonLanding}><img src='https://www.gifcen.com/wp-content/uploads/2021/04/earth-day-gif-11.gif' alt='Landing'
       className={style.landingGif}
       /></NavLink>
         </div>
